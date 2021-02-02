0b42a7c5930a83fdb6fdd489bc874c0f6e1ba14b
Stefanie Mantz <s.mantz@stud.uni-heidelberg.de>
2021-02-02 19:22:18 +0100

    Moved files in directory Exercise_2

--- notebooks/Exercise_2/Exercise_2.py
+++ notebooks/Exercise_2/Exercise_2.py
@@ -1,5 +1,5 @@
-from notebooks.count_aas import count_aas
-from notebooks.plot_aa_histogram import plot_aa_histogram
+from notebooks.Exercise_2.count_aas import count_aas
+from notebooks.Exercise_2.plot_aa_histogram import plot_aa_histogram
 
 counter_human = count_aas('uniprot_human.fasta')
 print(counter_human)
